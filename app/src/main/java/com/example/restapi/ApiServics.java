b78d53cf3b6ae2462b463f1c80e3d094a6c8edfa
Dell <mollanoman2019@gmail.com>
2020-07-16 23:20:32 +0600

    CoronaVirusAPI Project-V.1

---
+++ app/src/main/java/com/example/restapi/ApiServics.java
@@ -0,0 +1,10 @@
+package com.example.restapi;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiServics {
+    @GET("/countries/bangladesh")
+    Call<ModelClass> getData();
+
+}
