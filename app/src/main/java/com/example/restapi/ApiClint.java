f53abc84ce3254154d8bb0b10e62b490ee6aa7e7
Dell <mollanoman2019@gmail.com>
2020-07-16 23:30:37 +0600

    CoronaVirusAPI Project-V.2

---
+++ app/src/main/java/com/example/restapi/ApiClint.java
@@ -0,0 +1,31 @@
+package com.example.restapi;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClint {
+
+    private static final String BASE_URL = "https://coronavirus-19-api.herokuapp.com";
+
+    public static Retrofit getRetrofit() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .callTimeout(2, TimeUnit.MINUTES)
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS);
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create());
+
+        builder.client(httpClient.build());
+        Retrofit retrofit = builder.build();
+        return retrofit;
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/restapi/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.restapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    TextView totalCasesTextView;
+    List<ModelClass> arraylist;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        totalCasesTextView=findViewById(R.id.totalCaseId);
+            getUserList();
+
+    }
+
+    private void getUserList() {
+        try{
+
+            ApiServics servics=ApiClint.getRetrofit().create(ApiServics.class);
+            Call<ModelClass> call=servics.getData();
+            call.enqueue(new Callback<ModelClass>() {
+                @Override
+                public void onResponse(Call<ModelClass> call, Response<ModelClass> response) {
+                    if(response.isSuccessful()){
+                        List<ModelClass> userList= (List<ModelClass>) response.body();
+                        //int cases=  userList.get(0).getCases();
+                      //  totalCasesTextView.setText(cases);
+
+                    }
+                    else {
+
+
+                    }
+
+                }
+
+
+                @Override
+                public void onFailure(Call<ModelClass> call, Throwable t) {
+
+                }
+            });
+
+
+        }catch(Exception ex){
+
+
+
+
+        }
+    }
+}
